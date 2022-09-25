df203eb7f8fcb4f5942f52bc6404636ef547f4b8
Akshit1311 <a4748g@gmail.com>
2022-09-26 00:07:52 +0530

    add: filecoin logic

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const sendEpnsNotif = require("./epns");
+import express from "express";
+// const sendEpnsNotif = require("./epns");
+import { makeFileObjects } from "./filecoin.js";
 
 const app = express();
 
-app.post("/epns/send-notif", async (req, res) => {
+// app.post("/epns/send-notif", async (req, res) => {
+//   const { body } = req;
+
+//   try {
+//     const result = await sendEpnsNotif(body);
+
+//     console.log({ result });
+
+//     return res.status(200).send({ msg: "Success", data: result });
+//   } catch (error) {
+//     console.log({ error });
+//     return res.status(500).send({ msg: "Internal Server Error", error });
+//   }
+// });
+
+app.post("/filecoin", async (req, res) => {
   const { body } = req;
 
   try {
-    const result = await sendEpnsNotif(body);
+    const result = await makeFileObjects();
 
     console.log({ result });
 
